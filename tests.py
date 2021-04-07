ec6722010c374c0b58df0d6ba4f7dfaffc3a8152
malinoski <iuri.malinoski@g.globo>
2021-04-07 13:16:12 -0300

    Update tests

--- tests.py
+++ tests.py
@@ -2,13 +2,15 @@ from tasks import reverse
 from time import sleep
 
 print("# Test 1")
-result = reverse.delay('iuri')
-print(result.status)
+result = reverse.delay('iuri')  # or reverse.apply_async(args=['iuri'])
+print("Get the id: ", result.id)
+print("Get the status: ", result.status)
+print("Wait to status change...")
 sleep(6)
-print(result.status)
+print("# Get the new status", result.status)
 
 print("\n# Test 2")
 result = reverse.apply_async(args=['iuri'])
-print(result.status)
-sleep(6)
-print(result.status)
+print("Synchronous getting the response...")
+print("Response: ", result.get())
+
