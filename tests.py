c4c5cb15284bb5f588f1b96ea2a44bf6a4d2c276
malinoski <iuri.malinoski@g.globo>
2021-04-07 13:25:09 -0300

    Added new test

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from tasks import reverse
 from time import sleep
 
-print("# Test 1")
+print("# Test 1 - status")
 result = reverse.delay('iuri')  # or reverse.apply_async(args=['iuri'])
 print("Get the id: ", result.id)
 print("Get the status: ", result.status)
@@ -9,8 +9,13 @@ print("Wait to status change...")
 sleep(6)
 print("# Get the new status", result.status)
 
-print("\n# Test 2")
+print("\n# Test 2 - get")
 result = reverse.apply_async(args=['iuri'])
 print("Synchronous getting the response...")
 print("Response: ", result.get())
 
+print("\n# Test 3 - ready")
+result = reverse.apply_async(args=['iuri'])
+print("Finished processing? ", result.ready())
+sleep(6)
+print("Finished processing? ", result.ready())
